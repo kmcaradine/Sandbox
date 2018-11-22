2d898dfbecf2ecf45845ca3e09b99e0f1db9a069
Ken Caradine <kcqcone@gmail.com>
2018-11-21 23:26:21 -0600

    Remove printpretty() method to print to the report and revise build gradle

--- src/test/java/com/NetworkAPIs/SearchRepositories.java
+++ src/test/java/com/NetworkAPIs/SearchRepositories.java
@@ -9,22 +9,28 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
 import org.testng.ITestResult;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import javax.ws.rs.core.MediaType;
 import java.lang.reflect.Method;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 public class SearchRepositories extends SharedCode {
 
-    private RequestSpecBuilder builder;
+    private RequestSpecBuilder requestSpecBuilder ;
     private RequestSpecification requestSpec;
     private Response response;
     private String apiType = "";
     private String testName = "";
 
-    @BeforeSuite(alwaysRun = true)
+    @BeforeClass(alwaysRun = true)
     public void setUp(){
         setUpReport();
     }
@@ -40,23 +46,26 @@ public class SearchRepositories extends SharedCode {
 
         test = extent.createTest(testName,"SearchRepositories");
 
-        String baseURI = "https://api.github.com/search/repositories";
-        //Send the URL the report
+        //Send the Base URL and Base Path  the report
+        String baseURI = "https://api.github.com";
+        String basePath = "/search/repositories";
         test.log(Status.INFO, "Base URI: " + baseURI);
+        test.log(Status.INFO, "Base Path: " + basePath);
 
         //Build API request
-        builder = new RequestSpecBuilder();
-        builder.setBaseUri(baseURI);
-        builder.addQueryParam("q","tetris+language:assembly");
-        builder.addQueryParam("sort","stars");
-        builder.addQueryParam("order","desc");
-        requestSpec = builder.build();
+        requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setBaseUri(baseURI);
+        requestSpecBuilder.setBasePath(basePath);
+        requestSpecBuilder.addQueryParam("q","tetris+language:assembly");
+        requestSpecBuilder.addQueryParam("sort","stars");
+        requestSpecBuilder.addQueryParam("order","desc");
+        requestSpec = requestSpecBuilder.build();
 
         //Get API response
         response = getTheResponse(apiType, requestSpec);
 
         //Send the response to the report
-        test.log(Status.INFO, "Response Body: " + response.body().prettyPrint());
+        test.log(Status.INFO, "Response Body: " + response.body().asString());
 
         //Assert on api response
         assertThat(response.statusCode(), is(HttpStatus.SC_OK));
@@ -80,7 +89,7 @@ public class SearchRepositories extends SharedCode {
         }
     }
 
-    @AfterSuite(alwaysRun = true)
+    @AfterClass(alwaysRun = true)
     public void tearDown(){
 
         //Send result to the Extent report
